048bc948a9aa7db97e4c9dc666fbf9f549a73267
Samuel <samuelac7223@gmail.com>
2024-12-26 23:23:01 -0600

    build(app): Initial set up
    The project was set up with very good cli

---
+++ android/app/src/main/kotlin/sam/alcantara/irida/irida/qr/MainActivity.kt
@@ -0,0 +1,6 @@
+package sam.alcantara.irida.irida_qr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:irida_qr/app/app.dart';
+import 'package:irida_qr/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
